6fc574d34f77766a944223868d220d15d702b201
milogallii <itsmemjlo@protonmail.com>
2026-03-21 20:05:03 +0100

    autopush

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 export SNIPPETS_PATH="$HOME/.config/helix/snippets.toml"
+export EDITOR="helix"
 set -gx PATH $HOME/.cargo/bin $PATH
 set -U fish_greeting
 set -g fish_autosuggestion_enabled 0
@@ -9,7 +10,13 @@ set -gx OMNISHARP_USE_MODERN_NET true
 if status is-interactive
 end
 
+function compile_typst
+    typst compile $argv[1] out.pdf
+end
+
 alias cd='z'
+alias r='spf'
+alias tyc='compile_typst'
 abbr -a rmf 'rm -rf'
 abbr -a l 'eza -hlai'
 abbr -a ta 'tmux a'
@@ -19,5 +26,6 @@ abbr -a gps 'git add . && git commit -m "autopush" && git push'
 abbr -a gp 'git pull --rebase'
 abbr -a vpn_add 'nmcli con import type wireguard file'
 abbr -a p pulsemixer
+abbr -a pdf sioyek
 
 zoxide init fish | source
