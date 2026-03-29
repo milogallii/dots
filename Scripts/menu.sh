b04ff72c3e439a5607a93c2889b8b61bf3388142
milogallii <itsmemjlo@protonmail.com>
2026-03-29 16:36:04 +0200

    autopush

--- Scripts/menu.sh
+++ Scripts/menu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dmenu_run -i -fn "Iosevka Nerd Font:regular:pixelsize=24" -nb "#282828" -sb "#E0FF4F" -nf "#dddddd" -sf "#282828"
+dmenu_run -i -fn "Iosevka Nerd Font:regular:pixelsize=24" -nb "#282828" -sb "#ff7878" -nf "#dddddd" -sf "#282828"
