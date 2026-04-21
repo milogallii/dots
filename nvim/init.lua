65de0515b6e6c58c37ea4efda96fd98e04a5d90e
milogallii <itsmemjlo@protonmail.com>
2026-04-21 19:58:21 +0200

    autopush

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,20 +2,19 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 999
 vim.o.signcolumn = "yes:1"
-vim.o.expandtab = true      
-vim.o.shiftwidth = 4       
-vim.o.tabstop = 4          
-vim.o.smartindent = true   
-vim.o.termguicolors = true  
-vim.o.undofile = true       
-vim.o.cursorline = true     
-vim.o.timeoutlen = 300     
-
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.cursorline = true
+vim.o.timeoutlen = 150
 vim.opt.fillchars = { eob = " " }
 vim.opt.ignorecase = true
 vim.opt.wrap = false
-vim.opt.linebreak = true     
-vim.opt.breakindent = true  
+vim.opt.linebreak = true
+vim.opt.breakindent = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.g.mapleader = ' '
@@ -26,7 +25,7 @@ local opts = { noremap = true, silent = true }
 -- File
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
-map("n", "<leader>f", "<cmd>Oil<cr>", { desc = "File explorer" })
+map("n", "<leader>t", "<cmd>lua MiniTrailspace.trim() MiniTrailspace.trim_last_lines()<cr>", { desc = "Trim whitespaces" })
 
 -- Buffers
 map("n", "<leader>m", "<cmd>bnext<cr>", { desc = "Next buffer" })
@@ -38,7 +37,7 @@ map("n", "<leader>sv", "<cmd>vsplit<cr>", { desc = "Vertical split" })
 map("n", "<leader>sh", "<cmd>split<cr>", { desc = "Horizontal split" })
 
 -- Clear search highlight
-map("n", "<leader>h", "<cmd>nohlsearch<cr>", { desc = "Clear highlights" })
+map("n", "<leader>h", "<cad>nohlsearch<cr>", { desc = "Clear highlights" })
 
 -- Better window navigation
 map("n", "<C-h>", "<C-w>h", opts)
@@ -57,15 +56,39 @@ map("v", "<A-j>", ":m '>+1<cr>gv=gv", opts)
 map("v", "<A-k>", ":m '<-2<cr>gv=gv", opts)
 
 vim.pack.add {
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/nvim-telescope/telescope.nvim',
+    'https://github.com/nvim-mini/mini.nvim',
 	'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/nvim-treesitter/nvim-treesitter',
 	'https://github.com/stevearc/oil.nvim',
 	'https://github.com/rose-pine/neovim.git',
-	'https://github.com/nvim-lualine/lualine.nvim',
 }
 
-require('lualine').setup({
-    options = { theme = 'rose-pine' }
-})
 require("oil").setup()
+require('mini.basics').setup()
+require('mini.ai').setup()
+require('mini.pairs').setup()
+require('mini.pairs').setup()
+require('mini.completion').setup()
+require('mini.operators').setup()
+require('mini.surround').setup()
+require('mini.git').setup()
+require('mini.diff').setup()
+require('mini.trailspace').setup()
+require('mini.statusline').setup()
+require('mini.comment').setup({
+    mappings = {
+        comment_line = '<Space>c',
+        comment_visual = '<Space>c',
+    },
+})
+
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('rust-analyzer')
 vim.cmd.colorscheme('rose-pine')
+
+local builtin = require('telescope.builtin')
+map("n", "<leader>ff", builtin.find_files)
+map('n', '<leadr>fg', builtin.live_grep)
 
